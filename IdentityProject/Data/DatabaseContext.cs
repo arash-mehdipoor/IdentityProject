9318074f421e03a3f313859aa9fa26d63a34aebf
Arash Mehdipour <arash201221@gmail.com>
2021-12-02 22:43:38 +0330

    Add IdentityDbContext

---
+++ IdentityProject/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityProject.Data
+{
+    public class DatabaseContext : IdentityDbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
