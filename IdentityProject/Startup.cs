e293938d2396fa45a691002d845d1c57cf92cd23
Arash Mehdipour <arash201221@gmail.com>
2021-12-04 20:40:04 +0330

    Custom Identity Error

--- IdentityProject/Startup.cs
+++ IdentityProject/Startup.cs
@@ -1,19 +1,15 @@
 using Identity.Bugeto.Models.Entities;
 using IdentityProject.Data;
+using IdentityProject.Helpers;
 using IdentityProject.Models.Entities;
 using IdentityProject.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityProject
 {
@@ -33,7 +29,8 @@ namespace IdentityProject
             services.AddDbContext<DatabaseContext>(d => d.UseSqlServer(Configuration.GetConnectionString("IdentityCnn")));
             services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<DatabaseContext>()
-                .AddDefaultTokenProviders();
+                .AddDefaultTokenProviders()
+                .AddErrorDescriber<CustomIdentityError>();
             services.AddScoped<EmailServices>();
             services.Configure<IdentityOptions>(option =>
             {
